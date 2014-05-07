90cf4e579b9f994e8d67ded7aeb337bf0a6bdd2e
Richard Kiss <him@richardkiss.com>
2014-05-06 22:55:44 -0700

    Update setup.py description and classifiers. yapf error: setup.py invalid syntax (<unknown>, line 39) [gitreformat yapf-ify (github/ghtdak) on Fri Jul 15 12:50:26 2016] [from commit: 741333866a0ed776791a4ac11a19259b9e94a208]

--- setup.py
+++ setup.py
@@ -7,25 +7,42 @@ version = "0.41"
 setup(
     name="pycoin",
     version=version,
-    packages=[
-        "pycoin", "pycoin.convention", "pycoin.ecdsa", "pycoin.key",
-        "pycoin.tx", "pycoin.tx.script", "pycoin.serialize", "pycoin.services",
+    packages = [
+        "pycoin",
+        "pycoin.convention",
+        "pycoin.ecdsa",
+        "pycoin.key",
+        "pycoin.tx",
+        "pycoin.tx.script",
+        "pycoin.serialize",
+        "pycoin.services",
         "pycoin.scripts"
     ],
     author="Richard Kiss",
-    entry_points={'console_scripts':
-                  [
-                      'block = pycoin.scripts.block:main',
-                      'ku = pycoin.scripts.ku:main',
-                      'tx = pycoin.scripts.tx:main',
-                      'cache_tx = pycoin.scripts.cache_tx:main',
-                      'fetch_unspent = pycoin.scripts.fetch_unspent:main',
-                      ## these scripts are obsolete
-                      'genwallet = pycoin.scripts.genwallet:main',
-                      'spend = pycoin.scripts.spend:main',
-                      'bu = pycoin.scripts.bitcoin_utils:main',
-                  ]},
+    entry_points = { 'console_scripts':
+            [
+                'block = pycoin.scripts.block:main',
+                'ku = pycoin.scripts.ku:main',
+                'tx = pycoin.scripts.tx:main',
+                'cache_tx = pycoin.scripts.cache_tx:main',
+                'fetch_unspent = pycoin.scripts.fetch_unspent:main',
+                ## these scripts are obsolete
+                'genwallet = pycoin.scripts.genwallet:main',
+                'spend = pycoin.scripts.spend:main',
+                'bu = pycoin.scripts.bitcoin_utils:main',
+            ]
+        },
     author_email="him@richardkiss.com",
     url="https://github.com/richardkiss/pycoin",
     license="http://opensource.org/licenses/MIT",
-    description="A bunch of utilities that might be helpful when dealing with Bitcoin addresses and transactions.")
+    description="Utilities for Bitcoin and altcoin addresses and transaction manipulation."
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet',
+        'Topic :: Internet :: Bitcoin',
+        'Topic :: Internet :: Cryptocurrency',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],)
