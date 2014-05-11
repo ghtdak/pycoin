9a98216713d3fe8941344061597a5ec6fe90b2b0
Richard Kiss <him@richardkiss.com>
2014-05-10 22:53:08 -0700

    Remove unknown classifiers from setup.py.
    [gitreformat yapf-ify (github/ghtdak) on Fri Jul 15 12:50:41 2016]
    [from commit: 8045c8bab104c9c8a9a9af0c4c2f405fae0a7818]

--- setup.py
+++ setup.py
@@ -35,7 +35,5 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet',
-        'Topic :: Internet :: Bitcoin',
-        'Topic :: Internet :: Cryptocurrency',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],)
