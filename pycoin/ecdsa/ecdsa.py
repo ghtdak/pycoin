ed221a76920c6d2ae48fe99130323c8e4f0009d5
Richard Kiss <him@richardkiss.com>
2014-05-22 16:35:31 -0700

    Use native bit_length if it exists.
    [gitreformat yapf-ify (github/ghtdak) on Fri Jul 15 12:50:42 2016]
    [from commit: 4e525bb34a83cc821807b6861c82437be03b211c]

--- pycoin/ecdsa/ecdsa.py
+++ pycoin/ecdsa/ecdsa.py
@@ -32,13 +32,16 @@ import hmac
 
 from . import ellipticcurve, intbytes, numbertheory
 
+if hasattr(1, "bit_length"):
+    bit_length = int.bit_length
+else:
 
-def bit_length(self):
-    # Make this library compatible with python < 2.7
-    # https://docs.python.org/3.5/library/stdtypes.html#int.bit_length
-    s = bin(self)  # binary representation:  bin(-37) --> '-0b100101'
-    s = s.lstrip('-0b')  # remove leading zeros and minus sign
-    return len(s)  # len('100101') --> 6
+    def bit_length(self):
+        # Make this library compatible with python < 2.7
+        # https://docs.python.org/3.5/library/stdtypes.html#int.bit_length
+        s = bin(self)  # binary representation:  bin(-37) --> '-0b100101'
+        s = s.lstrip('-0b')  # remove leading zeros and minus sign
+        return len(s)  # len('100101') --> 6
 
 
 def deterministic_generate_k(generator_order,
