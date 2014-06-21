375305ac9bbed6780d2cf310d7175b9f153102f4
Richard Kiss <him@richardkiss.com>
2014-06-20 19:08:49 -0700

    Forgot a file (and an opcode).
    [gitreformat yapf-ify (github/ghtdak) on Fri Jul 15 12:50:50 2016]
    [from commit: fe062f37482b6f55b1007d5d826adf6580a06974]

--- pycoin/tx/script/microcode.py
+++ pycoin/tx/script/microcode.py
@@ -47,6 +47,10 @@ do_OP_NOP = do_OP_NOP1 = do_OP_NOP2 = do_OP_NOP3 = do_OP_NOP4 = do_OP_NOP5 = lam
 do_OP_NOP6 = do_OP_NOP7 = do_OP_NOP8 = do_OP_NOP9 = do_OP_NOP10 = lambda s: None
 
 
+def do_OP_0(stack):
+    stack.append(VCH_FALSE)
+
+
 def do_OP_VERIFY(stack):
     pass
 
