9a679c65ef0e92c6bcdd138f3bb904944ffb6d47
Richard Kiss <him@richardkiss.com>
2014-07-16 18:16:44 -0700

    Fix syntax error in block.py script.
    [gitreformat yapf-ify (github/ghtdak) on Fri Jul 15 12:50:53 2016]
    [from commit: 7e3cae248186d431cb0a8360ff88358cc2fe72c2]

--- pycoin/scripts/block.py
+++ pycoin/scripts/block.py
@@ -13,19 +13,24 @@ def dump_block(block, network):
     print("%d bytes   block hash %s" % (len(blob), block.id()))
     print("version %d" % block.version)
     print("prior block hash %s" % b2h_rev(block.previous_block_hash))
-    print("merkle root %s" % b2h(block.merkle_root)
-    print("timestamp %s" % datetime.datetime.utcfromtimestamp(block.timestamp).isoformat())
+    print("merkle root %s" % b2h(block.merkle_root))
+    print("timestamp %s" %
+          datetime.datetime.utcfromtimestamp(block.timestamp).isoformat())
     print("difficulty %d" % block.difficulty)
     print("nonce %s" % block.nonce)
-    print("%d transaction%s" % (len(block.txs), "s" if len(block.txs) != 1 else ""))
+    print("%d transaction%s" % (len(block.txs), "s"
+                                if len(block.txs) != 1 else ""))
     for idx, tx in enumerate(block.txs):
         print("Tx #%d:" % idx)
         dump_tx(tx, netcode=network)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Dump a block in human-readable form.")
-    parser.add_argument("block_bin", nargs="+", type=argparse.FileType('rb'),
+    parser = argparse.ArgumentParser(
+        description="Dump a block in human-readable form.")
+    parser.add_argument("block_bin",
+                        nargs="+",
+                        type=argparse.FileType('rb'),
                         help='The file containing the binary block.')
 
     args = parser.parse_args()
@@ -34,6 +39,7 @@ def main():
         block = Block.parse(f)
         dump_block(block, network='BTC')
         print('')
+
 
 if __name__ == '__main__':
     main()
