5abb514a07a5b21b223d496004f88f8a72443646
Marek Miller <marek.l.miller@gmail.com>
2015-01-13 13:28:41 +0100

    Pass the correct script to Tx.py:signature_for_hash_type_f()
    This make the tests/tx_test.py pass and solves the problem exposed in ec69967.
    
    [gitreformat yapf-ify (github/ghtdak) on Fri Jul 15 12:51:32 2016]
    [from commit: c441ea75885167a809ae402d03e1066ed89d3804]

--- pycoin/tx/script/vm.py
+++ pycoin/tx/script/vm.py
@@ -166,7 +166,8 @@ def eval_script(script,
                 sig_ok = VCH_TRUE
                 for sig_blob in sig_blobs:
                     sig_pair, signature_type = parse_signature_blob(sig_blob)
-                    signature_hash = signature_for_hash_type_f(signature_type)
+                    signature_hash = signature_for_hash_type_f(signature_type,
+                                                               script=script)
 
                     ppp = ecdsa.possible_public_pairs_for_signature(
                         ecdsa.generator_secp256k1, signature_hash, sig_pair)
