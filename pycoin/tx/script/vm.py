d7bc391143c6ab6d323a96887b307be3e6c743e5
Richard Kiss <him@richardkiss.com>
2015-01-31 20:29:40 -0800

    Merge Marek Miller's fix for multisig.
    [gitreformat yapf-ify (github/ghtdak) on Fri Jul 15 12:51:34 2016]
    [from commit: 93334719719a6f11144425030594250432b33840]

--- pycoin/tx/script/vm.py
+++ pycoin/tx/script/vm.py
@@ -132,7 +132,8 @@ def eval_script(script,
                 sig_pair, signature_type = parse_signature_blob(stack.pop())
                 if expected_hash_type not in (None, signature_type):
                     raise ScriptError("wrong hash type")
-                signature_hash = signature_for_hash_type_f(signature_type)
+                signature_hash = signature_for_hash_type_f(signature_type,
+                                                           script=script)
                 if ecdsa.verify(ecdsa.generator_secp256k1, public_pair,
                                 signature_hash, sig_pair):
                     stack.append(VCH_TRUE)
@@ -166,7 +167,8 @@ def eval_script(script,
                 sig_ok = VCH_TRUE
                 for sig_blob in sig_blobs:
                     sig_pair, signature_type = parse_signature_blob(sig_blob)
-                    signature_hash = signature_for_hash_type_f(signature_type)
+                    signature_hash = signature_for_hash_type_f(signature_type,
+                                                               script=script)
 
                     ppp = ecdsa.possible_public_pairs_for_signature(
                         ecdsa.generator_secp256k1, signature_hash, sig_pair)
