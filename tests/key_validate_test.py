e451618b6137732b59580f811be23c227b52c99d
Matt Bogosian <mtb19@columbia.edu>
2015-02-25 08:40:10 -0800

    Fix #83. Key.is_private -> Key.is_private() where appropriate.
    [gitreformat yapf-ify (github/ghtdak) on Fri Jul 15 12:51:43 2016]
    [from commit: 8044714c5ebab32c77fd5fdbf39ea740c1b897b3]

--- tests/key_validate_test.py
+++ tests/key_validate_test.py
@@ -2,6 +2,9 @@
 
 import unittest
 
+# This is necessary for the import Wallet statement below to work?
+import tests.bip32_test
+
 from pycoin.block import Block
 from pycoin.encoding import hash160_sec_to_bitcoin_address
 from pycoin.key import Key
@@ -123,3 +126,20 @@ class KeyUtilsTest(unittest.TestCase):
                 self.assertEqual(
                     is_public_bip32_valid(a, allowable_netcodes=NETWORK_NAMES),
                     None)
+
+    def test_repr(self):
+        key = Key(secret_exponent=273, netcode='XTN')
+
+        address = key.address()
+        pub_k = Key.from_text(address)
+        self.assertEqual(repr(pub_k), '<mhDVBkZBWLtJkpbszdjZRkH1o5RZxMwxca>')
+
+        wif = key.wif()
+        priv_k = Key.from_text(wif)
+        self.assertEqual(
+            repr(priv_k),
+            'private_for <0264e1b1969f9102977691a40431b0b672055dcf31163897d996434420e6c95dc9>')
+
+
+if __name__ == '__main__':
+    unittest.main()
